08ec19a6530670811faa4d2cd79996d6d9fb24de
Festongithub <fkemboik@gmail.com>
2024-10-16 02:23:32 -0700

    Reddis

---
+++ utils/redis.js
@@ -0,0 +1,37 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+
+    this.client.on('connect', () => {
+     console.log('Redis client connected to the server');
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const val = await this.getAsync(key);
+    return val;
+  }
+
+  async set(key, value, dur) {
+    this.client.setex(key, dur, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
