a4c544f5f61d4792fdef539a3d936c7fa6470f70
Festongithub <fkemboik@gmail.com>
2024-10-16 04:04:19 -0700

    Server

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const router = require("./routes/index");
 const server = express();
 const PORT = process.env.PORT ? process.env.PORT : 5000;
 
-server.use(express.join());
+server.use(express.json());
 server.use(router);
 
 server.listen(PORT, () => 
-	console.log(`Server running on port ${PORT}`);
-});
-
+	console.log(`Server running on port ${PORT}`)
+);
