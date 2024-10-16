d7009ab3e1e20cb1125d808cb19bb2608c311ac9
Festongithub <fkemboik@gmail.com>
2024-10-16 05:47:26 -0700

    Day one

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,19 +1,21 @@
-import  redisClient from '../utils/redis'
+import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static getStatus(req, res) {
-    if (redisClient.isAlive() && dbClient.isAlive()) {
-      res.json({ "redis": "true", "db": "true" });
-      res.end();
-    }
+  static getStatus(request, response) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    response.status(200).send(status);
   }
 
-  static async getStats(req, res) {
-    const users = await dbClient.nbUsers();
-    const files = await dbClient.nbFiles();
-    res.json({ users, files });
-    res.end();
+  static async getStats(request, response) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    response.status(200).send(stats);
   }
 }
-module.exports = AppController;
+export default AppController;
