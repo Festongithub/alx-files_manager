a31abff95cbb64ae1b74fae165de13a62391f720
Festongithub <fkemboik@gmail.com>
2024-10-17 01:14:12 -0700

    BD

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
-#!/usr/bin/node
+import express from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
+function controllerRouting(app) {
+  const router = express.Router();
+  app.use('/', router);
 
-const express = require('express');
-const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController');
-const router = express.Router();
+  router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
+  });
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
+  router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);
+  });
+}
 
-module.exports = router;
+export default controllerRouting;
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-#!/usr/bin/node
+import express from 'express';
+import controllerRouting from './routes/index';
 
+const app = express();
+const port = process.env.PORT || 5000;
 
-const express = require("express");
-const router = require("./routes/index");
+app.use(express.json());
 
-const server = express();
-const PORT = process.env.PORT ? process.env.PORT : 5000;
+controllerRouting(app);
 
-server.use(express.json());
-server.use(router);
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+});
 
-server.listen(PORT, () => 
-	console.log(`Server running on port ${PORT}`)
-);
+export default app;
