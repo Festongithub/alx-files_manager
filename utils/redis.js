b1ea4bff5d0dd028e5438365b5ad02cc2b7afdee
Festongithub <fkemboik@gmail.com>
2024-10-17 02:59:19 -0700

    Ser

--- utils/redis.js
+++ utils/redis.js
@@ -1,21 +1,37 @@
-import { createClient } from 'redis';
-import { promisify } from  'util';
+import redis  from 'redis';
+import { promisify } from 'util';
 
-class RedisClient{
-	constructor() {
-		this.client = redis.createClient();
-		this.client.on('error', (err) => {
-			console.log(err));
-		});
-	}
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
 
-	isAlive() {
-		this.client.connected;
-	}
+    this.client.on('error', (err) => {
+      console.log(err);
+    });
+    this.client.on('connect', () => {
+      console.log('Redis client connected to the server');
+    });
+  }
 
-	async function  get(key) {
-		const value = await this.getAsync(key);
-		return value;
-	}
+  isAlive() {
+    this.client.connected;
+  }
 
-	async set(key,duration, )
+  async get(key) {
+    const value = await this.getAsync(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
